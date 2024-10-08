84d3cb858128c3dad0e5d3b8aec7ecc8cdd9ae3c
Daesu <rlaeotn6@gmail.com>
2024-10-08 09:56:12 -0400

    working on hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewChecked{
+  images = [89, 70, 18].map((n) => `https://picsum.photos/id/${n}/1800/855/`);
 
+  ngAfterViewChecked(): void {
+  }
 }
