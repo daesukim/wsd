92984cab2025ec5c4ceaa1e336c08ed54aee65f8
Daesu <rlaeotn6@gmail.com>
2024-10-08 11:26:47 -0400

    cleanup

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -8,18 +8,6 @@ import { NgbCarousel, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
-export class HeroComponent implements AfterViewChecked{
+export class HeroComponent{
   images = [89, 70, 18].map((n) => `https://picsum.photos/id/${n}/1800/855/`);
-  @ViewChild('tracker') trackerRef?: ElementRef;
-
-  ngAfterViewChecked(): void {
-  }
-
-  mouseEnter(event: MouseEvent){
-    const clientX = event.clientX;
-    const clientY = event.clientY;
-    if (this.trackerRef?.nativeElement.style) {
-      this.trackerRef.nativeElement.style.top = `${clientY / 20}px`;
-  }
-  }
 }
